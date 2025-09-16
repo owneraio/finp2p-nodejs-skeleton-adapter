2df4bd0702e88bd9692edfd6127fe777ff0268e6
Andrey Koltochnik <ankolt@gmail.com>
2025-09-16 14:29:47 +0300

    Export components and paths (#27)
    * Export components and paths
    
    * Bump client version

--- finp2p-client/src/finapi/index.ts
+++ finp2p-client/src/finapi/index.ts
@@ -1,4 +1,6 @@
+import {components as FinAPIComponents, paths as FinAPIPaths} from './model-gen'
+import {components as OpComponents, paths as OpPaths} from './op-model-gen'
+
+export {FinAPIComponents, FinAPIPaths, OpComponents, OpPaths}
 export * from './finapi.client'
 export * from './model-gen'
-export {components as FinAPIComponents, paths as FinAPIPaths} from './model-gen'
-export {components as OpComponents, paths as OpPaths} from './op-model-gen'
