3338fb35e733f479dd0719ed9b75cfb3f0e7ac35
Shakhzod Ikromov <shakhzod.ikromov@ownera.io>
2025-10-21 18:53:54 +0300

    Use NPM_TOKEN for downloading GitHub Packages

--- sample-adapter/build-docker.sh
+++ sample-adapter/build-docker.sh
@@ -2,11 +2,14 @@
 
 set -euo pipefail
 
-docker build -t finp2p-nodejs-skeleton-adapter-migrations:latest -f ./db-migrations/Dockerfile .
+docker build -t finp2p-nodejs-skeleton-adapter-migrations:latest \
+  --secret id=npm_token,env=GITHUB_TOKEN \
+  -f ./db-migrations/Dockerfile .
 docker tag finp2p-nodejs-skeleton-adapter-migrations:latest localhost:5000/finp2p-nodejs-skeleton-adapter-migrations:latest
 docker push localhost:5000/finp2p-nodejs-skeleton-adapter-migrations:latest
 
-docker build -t finp2p-nodejs-skeleton-adapter:latest -f Dockerfile .
+docker build -t finp2p-nodejs-skeleton-adapter:latest \
+  --secret id=npm_token,env=GITHUB_TOKEN \
+  -f Dockerfile .
 docker tag finp2p-nodejs-skeleton-adapter:latest localhost:5000/finp2p-nodejs-skeleton-adapter:latest
 docker push localhost:5000/finp2p-nodejs-skeleton-adapter:latest
-
