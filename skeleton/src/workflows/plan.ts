8dbf9a1b9641af45f4e22dc13118d674748e1649
Shakhzod Ikromov <shakhzod.ikromov@ownera.io>
2025-10-27 13:47:40 +0200

    Additional instructions

--- skeleton/src/workflows/plan.ts
+++ skeleton/src/workflows/plan.ts
@@ -1,5 +1,9 @@
 import {
+  Asset,
   CommonService,
+  DepositAsset,
+  DepositOperation,
+  Destination,
   EscrowService,
   HealthService,
   OperationStatus,
@@ -7,12 +11,14 @@ import {
   PlanApprovalService,
   PlanApprovalStatus,
   ReceiptOperation,
+  Signature,
+  Source,
   TokenService,
 } from "../services";
 import { WorkflowStorage, Operation as StorageOperation } from "./storage";
 
 export class WorkflowPlanApprovalService
-  implements PlanApprovalService, CommonService
+  implements PlanApprovalService, CommonService, PaymentService
 {
   constructor(
     private storage: WorkflowStorage,
@@ -23,6 +29,38 @@ export class WorkflowPlanApprovalService
     private planService: PlanApprovalService,
     private tokenService: TokenService,
   ) {}
+
+  async getDepositInstruction(
+    ...args: Parameters<PaymentService["getDepositInstruction"]>
+  ): ReturnType<PaymentService["getDepositInstruction"]> {
+    return await this.callMethod(
+      this.paymentService,
+      this.paymentService.getDepositInstruction,
+      {
+        args,
+        idempotencyKey: args[0],
+        dbMethod: "getDepositInstruction",
+        dbStatus: depositOperationStatusToDb,
+        cidMapper: depositOperationCidMapper,
+      },
+    );
+  }
+
+  async payout(
+    ...args: Parameters<PaymentService["payout"]>
+  ): ReturnType<PaymentService["payout"]> {
+    return await this.callMethod(
+      this.paymentService,
+      this.paymentService.payout,
+      {
+        args,
+        idempotencyKey: args[0],
+        dbMethod: "payout",
+        dbStatus: receiptOperationStatusToDb,
+        cidMapper: receiptOperationCidMapper,
+      },
+    );
+  }
 
   async getReceipt(id: string): Promise<ReceiptOperation> {
     return await this.commonService.getReceipt(id);
@@ -35,18 +73,44 @@ export class WorkflowPlanApprovalService
 
     switch (storageOperation.method) {
       case "approvePlan":
-        const underlying = await this.commonService.operationStatus(
+        const approvePlan = await this.commonService.operationStatus(
           (storageOperation.outputs as { correlationId: string }).correlationId,
         );
-        if (underlying.operation !== "approval")
+        if (approvePlan.operation !== "approval")
           throw new Error(
-            `Expected PlanApprovalStatus, but found ${underlying.operation}`,
+            `Expected PlanApprovalStatus, but found ${approvePlan.operation}`,
           );
         await this.storage.changeStatus(
           cid,
-          planApprovalStatusToDb(underlying),
+          planApprovalStatusToDb(approvePlan),
         );
-        return planApprovalCidMapper(storageOperation, underlying);
+        return planApprovalCidMapper(storageOperation, approvePlan);
+      case "getDepositInstruction":
+        const deposit = await this.commonService.operationStatus(
+          (storageOperation.outputs as { correlationId: string }).correlationId,
+        );
+        if (deposit.operation !== "deposit")
+          throw new Error(
+            `Expected DepositOperation, but found ${deposit.operation}`,
+          );
+        await this.storage.changeStatus(
+          cid,
+          depositOperationStatusToDb(deposit),
+        );
+        return depositOperationCidMapper(storageOperation, deposit);
+      case "payout":
+        const payout = await this.commonService.operationStatus(
+          (storageOperation.outputs as { correlationId: string }).correlationId,
+        );
+        if (payout.operation !== "receipt")
+          throw new Error(
+            `Expected DepositOperation, but found ${payout.operation}`,
+          );
+        await this.storage.changeStatus(
+          cid,
+          receiptOperationStatusToDb(payout),
+        );
+        return receiptOperationCidMapper(storageOperation, payout);
       default:
         throw new Error(`Unexpected method stored ${storageOperation.method}`);
     }
@@ -125,6 +189,68 @@ const planApprovalCidMapper = (
     case "pending":
       return {
         ...planStatus,
+        correlationId: storage.cid,
+      };
+  }
+};
+
+const depositOperationStatusToDb = (
+  result: DepositOperation,
+): StorageOperation["status"] => {
+  switch (result.type) {
+    case "pending":
+      return "in_progress";
+    case "failure":
+      return "failed";
+    case "success":
+      return "succeeded";
+    default:
+      return "unknown";
+  }
+};
+
+const depositOperationCidMapper = (
+  storage: StorageOperation,
+  result: DepositOperation,
+): DepositOperation => {
+  switch (result.type) {
+    case "success":
+    case "failure":
+      return result;
+    case "pending":
+      return {
+        ...result,
+        correlationId: storage.cid,
+      };
+  }
+};
+
+const receiptOperationStatusToDb = (
+  result: ReceiptOperation,
+): StorageOperation["status"] => {
+  switch (result.type) {
+    case "success":
+      return "succeeded";
+    case "pending":
+      return "in_progress";
+    case "failure":
+      return "failed";
+    default:
+      return "unknown";
+  }
+};
+
+const receiptOperationCidMapper = (
+  storage: StorageOperation,
+  result: ReceiptOperation,
+): ReceiptOperation => {
+  switch (result.type) {
+    case "failure":
+    case "success":
+      return result;
+    case "pending":
+      return {
+        ...result,
         correlationId: storage.cid,
       };
   }
