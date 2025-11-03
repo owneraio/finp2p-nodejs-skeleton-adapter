38b2d98c210e74be0badea282236da41845266e1
Shakhzod Ikromov <shakhzod.ikromov@ownera.io>
2025-11-03 18:05:44 +0200

    Fix logger changes

--- finp2p-adapter-models/src/logger.ts
+++ finp2p-adapter-models/src/logger.ts
@@ -1,36 +1,36 @@
 export interface Logger {
   debug(...args: unknown[]): void;
   info(...args: unknown[]): void;
-  warn(...args: unknown[]): void;
+  warning(...args: unknown[]): void;
   error(...args: unknown[]): void;
 }
 
-export class ConsoleLogger implements Logger {
-  private levelOrder = ['debug', 'info', 'warn', 'error'] as const;
+const LEVEL_ORDER = ['debug', 'info', 'warn', 'error'] as const;
 
+export class ConsoleLogger implements Logger {
   private currentLevelIndex: number;
 
-  constructor(level: 'debug' | 'info' | 'warn' | 'error' = 'info') {
-    this.currentLevelIndex = this.levelOrder.indexOf(level);
+  constructor(level: typeof LEVEL_ORDER[number] = 'info') {
+    this.currentLevelIndex = LEVEL_ORDER.indexOf(level);
   }
 
-  private shouldLog(level: typeof this.levelOrder[number]) {
-    return this.levelOrder.indexOf(level) >= this.currentLevelIndex;
+  private shouldLog(level: typeof LEVEL_ORDER[number]) {
+    return LEVEL_ORDER.indexOf(level) >= this.currentLevelIndex;
   }
 
-  debug(...args: unknown[]): void {
+  debug(...args: unknown[]) {
     if (this.shouldLog('debug')) console.debug('[DEBUG]', ...args);
   }
 
-  info(...args: unknown[]): void {
+  info(...args: unknown[]) {
     if (this.shouldLog('info')) console.info('[INFO]', ...args);
   }
 
-  warn(...args: unknown[]): void {
+  warning(...args: unknown[]) {
     if (this.shouldLog('warn')) console.warn('[WARN]', ...args);
   }
 
-  error(...args: unknown[]): void {
+  error(...args: unknown[]) {
     if (this.shouldLog('error')) console.error('[ERROR]', ...args);
   }
 }
