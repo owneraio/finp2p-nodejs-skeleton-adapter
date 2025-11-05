093387f3ae3c2b92b27d3353bf5345cdde6ab2c0
Shakhzod Ikromov <aabbcc.double@gmail.com>
2025-11-05 17:11:20 +0200

    [Task] Improve Dockerfile (#65)

--- sample-adapter/build-docker.sh
+++ sample-adapter/build-docker.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-docker build -t finp2p-nodejs-skeleton-adapter:latest -f Dockerfile .
+set -euo pipefail
+
+docker build -t finp2p-nodejs-skeleton-adapter:latest \
+  --secret id=npm_token,env=GITHUB_TOKEN \
+  -f Dockerfile .
 docker tag finp2p-nodejs-skeleton-adapter:latest localhost:5000/finp2p-nodejs-skeleton-adapter:latest
 docker push localhost:5000/finp2p-nodejs-skeleton-adapter:latest
-
