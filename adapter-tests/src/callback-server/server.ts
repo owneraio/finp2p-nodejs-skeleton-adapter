52e4bd1c0c87930a8f8dc9036e5f660e1b7fec18
Shakhzod Ikromov <shakhzod.ikromov@ownera.io>
2025-11-20 12:48:25 +0200

    Minimal callback support

---
+++ adapter-tests/src/callback-server/server.ts
@@ -0,0 +1,62 @@
+import express from 'express';
+import getPort from 'get-port';
+import { sleep } from '../utils/utils';
+
+const register = (app: express.Application, operationsCache: Map<string, any>) => {
+  app.post('/operations/callback/:cid', async (req, res) => {
+    operationsCache.set(req.params.cid, req.body);
+  });
+};
+
+export class CallbackServer {
+  private readonly parkedMark = Symbol('parked');
+
+  constructor(
+    public readonly address: string,
+    public readonly app: express.Application,
+    private readonly operationsCache: Map<string, any>,
+  ) { }
+
+  expectLater(cid: string) {
+    this.operationsCache.set(cid, this.parkedMark);
+  }
+
+  isExpectedLater(cid: string) {
+    return this.operationsCache.get(cid) === this.parkedMark;
+  }
+
+  async operation<T>(cid: string): Promise<T> {
+    for (let i = 0; i < 30; i++) {
+      const result = this.operationsCache.get(cid);
+      if (result === this.parkedMark) {
+        await sleep(500);
+        continue;
+      }
+      if (result !== undefined) {
+        return result;
+      }
+    }
+
+    throw new Error('Expected callback in reasonable time, but not received');
+  }
+}
+
+export const create = async (): Promise<CallbackServer> => {
+  const operationsCache = new Map<string, any>();
+  const port = await getPort();
+  const app = express();
+  app.use(express.json({ limit: '50mb' }));
+  register(app, operationsCache);
+
+  await new Promise<void>((resolve, reject) => {
+    app.listen(port, error => {
+      if (!error) {
+        resolve();
+      } else {
+        reject(error);
+      }
+    });
+  });
+
+  return new CallbackServer(`http://localhost:${port}`, app, operationsCache);
+};
