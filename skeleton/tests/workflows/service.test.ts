c9793185dd27c9699a6af7d24f298bfc8f4c0516
Shakhzod Ikromov <shakhzod.ikromov@ownera.io>
2025-11-24 17:21:17 +0200

    Allow restart

--- skeleton/tests/workflows/service.test.ts
+++ skeleton/tests/workflows/service.test.ts
@@ -1,6 +1,7 @@
 import {
   approvedPlan,
   OperationStatus,
+  rejectedPlan,
   successfulAssetCreation,
 } from "@owneraio/finp2p-adapter-models";
 import {
@@ -16,7 +17,9 @@ describe("Service operation tests", () => {
     connectionString: "",
     cleanup: () => Promise.resolve(),
   };
-  let storage = (): Storage => { throw new Error('Not initialized yet') };
+  let storage = (): Storage => {
+    throw new Error("Not initialized yet");
+  };
   beforeEach(async () => {
     // @ts-ignore
     container = await global.startPostgresContainer();
@@ -26,11 +29,11 @@ describe("Service operation tests", () => {
       gooseExecutablePath: await global.whichGoose(),
       migrationListTableName: "finp2p_nodejs_skeleton_migrations",
     });
-    const s = new Storage(container)
-    storage = () => s
+    const s = new Storage(container);
+    storage = () => s;
   });
   afterEach(async () => {
-    await storage().closeConnections()
+    await storage().closeConnections();
     await container.cleanup();
   });
 
@@ -106,41 +109,173 @@ describe("Service operation tests", () => {
     expect(operation.outputs).toMatch("connect to RPC");
   });
 
-  test('calling the same inputs will result cached response', async () => {
+  test("calling the same inputs will result cached response", async () => {
     class Service {
-      callCount = new Map<string, number>()
+      callCount = new Map<string, number>();
 
-      async approve(idempotencyKey: string, planId: string): Promise<OperationStatus> {
-        const pseudoKey = JSON.stringify([idempotencyKey, planId])
-        const value = this.callCount.get(pseudoKey) ?? 0
-        this.callCount.set(pseudoKey, value + 1)
-        return approvedPlan()
+      async approve(
+        idempotencyKey: string,
+        planId: string,
+      ): Promise<OperationStatus> {
+        const pseudoKey = JSON.stringify([idempotencyKey, planId]);
+        const value = this.callCount.get(pseudoKey) ?? 0;
+        this.callCount.set(pseudoKey, value + 1);
+        return approvedPlan();
       }
     }
 
-    const service = new Service()
+    const service = new Service();
     const proxied = createServiceProxy(storage(), undefined, service, {
-      name: 'approve', operation: 'approval' })
+      name: "approve",
+      operation: "approval",
+    });
 
-    const idempotencyKey = Math.random().toString(36)
-    const planId = Math.random().toString()
-    expect(service.callCount.get(JSON.stringify([idempotencyKey, planId]))).toBeUndefined()
-    await expect(proxied.approve(idempotencyKey, planId)).resolves.toBeDefined()
-    expect(service.callCount.get(JSON.stringify([idempotencyKey, planId]))).toBe(1)
+    const idempotencyKey = Math.random().toString(36);
+    const planId = Math.random().toString();
+    expect(
+      service.callCount.get(JSON.stringify([idempotencyKey, planId])),
+    ).toBeUndefined();
+    await expect(
+      proxied.approve(idempotencyKey, planId),
+    ).resolves.toBeDefined();
+    expect(
+      service.callCount.get(JSON.stringify([idempotencyKey, planId])),
+    ).toBe(1);
 
     // Duplicating the request
-    await expect(proxied.approve(idempotencyKey, planId)).resolves.toBeDefined()
-    expect(service.callCount.get(JSON.stringify([idempotencyKey, planId]))).toBe(1)
+    await expect(
+      proxied.approve(idempotencyKey, planId),
+    ).resolves.toBeDefined();
+    expect(
+      service.callCount.get(JSON.stringify([idempotencyKey, planId])),
+    ).toBe(1);
 
-    const otherIdempotencyKey = Math.random().toString(36)
-    expect(service.callCount.get(JSON.stringify([otherIdempotencyKey, planId]))).toBeUndefined()
-    await expect(proxied.approve(otherIdempotencyKey, planId)).resolves.toBeDefined()
-    expect(service.callCount.get(JSON.stringify([otherIdempotencyKey, planId]))).toBe(1)
+    const otherIdempotencyKey = Math.random().toString(36);
+    expect(
+      service.callCount.get(JSON.stringify([otherIdempotencyKey, planId])),
+    ).toBeUndefined();
+    await expect(
+      proxied.approve(otherIdempotencyKey, planId),
+    ).resolves.toBeDefined();
+    expect(
+      service.callCount.get(JSON.stringify([otherIdempotencyKey, planId])),
+    ).toBe(1);
 
     // Duplicating the earlier request
-    await expect(proxied.approve(idempotencyKey, planId)).resolves.toBeDefined()
-    expect(service.callCount.get(JSON.stringify([idempotencyKey, planId]))).toBe(1)
+    await expect(
+      proxied.approve(idempotencyKey, planId),
+    ).resolves.toBeDefined();
+    expect(
+      service.callCount.get(JSON.stringify([idempotencyKey, planId])),
+    ).toBe(1);
 
-    expect(service.callCount.size).toBe(2)
-  })
+    expect(service.callCount.size).toBe(2);
+  });
+
+  test("interrupted methods should be restarted", async () => {
+    class Service {
+      async createAsset(
+        idempotencyKey: string,
+        decimals: number,
+        amount: string,
+      ): Promise<OperationStatus> {
+        return successfulAssetCreation({
+          tokenId: idempotencyKey,
+          reference: {
+            type: "ledgerReference",
+            tokenStandard: "createAsset",
+            network: decimals.toString(),
+            address: amount,
+            additionalContractDetails: {
+              allowanceRequired: false,
+              finP2POperatorContractAddress: "0x000",
+            },
+          },
+        });
+      }
+
+      async rejectPlan(
+        idempotencyKey: string,
+        planId: string,
+      ): Promise<OperationStatus> {
+        if (idempotencyKey === "crash-idempotency-key-2")
+          throw new Error("OSS failed to return proper data");
+
+        return rejectedPlan(404, [idempotencyKey, planId].join(":"));
+      }
+    }
+
+    const [row1, inserted1] = await storage().insert({
+      cid: "old-cid",
+      inputs: ["fake-idempotency-key-1", 5, "655000"],
+      method: "createAsset",
+      outputs: {},
+      status: "in_progress",
+    });
+    expect(inserted1).toBe(true);
+
+    const [row2, inserted2] = await storage().insert({
+      cid: "new-cid",
+      inputs: ["should-fail-idempotency", "plan-id-0"],
+      method: "rejectPlan",
+      outputs: {},
+      status: "in_progress",
+    });
+    expect(inserted2).toBe(true);
+
+    const [row3, inserted3] = await storage().insert({
+      cid: "newer-cid",
+      inputs: ["crash-idempotency-key-2", "plan-id-0"],
+      method: "rejectPlan",
+      outputs: {},
+      status: "in_progress",
+    });
+    expect(inserted3).toBe(true);
+
+    const service = new Service();
+    const proxied = createServiceProxy(
+      storage(),
+      undefined,
+      service,
+      { name: "createAsset", operation: "createAsset" },
+      { name: "rejectPlan", operation: "approval" },
+    );
+    await setTimeoutPromise(5000);
+
+    await expect(storage().operation(row1.cid)).resolves.toEqual({
+      cid: "old-cid",
+      inputs: ["fake-idempotency-key-1", 5, "655000"],
+      method: "createAsset",
+      outputs: successfulAssetCreation({
+        tokenId: "fake-idempotency-key-1",
+        reference: {
+          type: "ledgerReference",
+          tokenStandard: "createAsset",
+          network: (5).toString(),
+          address: "655000",
+          additionalContractDetails: {
+            allowanceRequired: false,
+            finP2POperatorContractAddress: "0x000",
+          },
+        },
+      }),
+      status: "succeeded",
+      created_at: expect.any(Date),
+      updated_at: expect.any(Date),
+    });
+
+    await expect(storage().operation(row2.cid)).resolves.toEqual({
+      ...row2,
+      updated_at: expect.any(Date),
+      outputs: rejectedPlan(404, "should-fail-idempotency:plan-id-0"),
+      status: "failed",
+    });
+
+    await expect(storage().operation(row3.cid)).resolves.toEqual({
+      ...row3,
+      updated_at: expect.any(Date),
+      outputs: expect.stringContaining("OSS failed to return proper data"),
+      status: "failed",
+    });
+  });
 });
